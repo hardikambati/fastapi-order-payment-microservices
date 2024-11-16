cc85de4f66d6fc8a3c10c3a0aca1873e68a44051
hardikanbati <hardikambati69@gmail.com>
2024-11-16 19:59:07 +0530

    product service init

---
+++ orders/api/external_services/product_service.py
@@ -0,0 +1,8 @@
+
+class ProductService:
+    def __init__(self, id_list: list):
+        self.id_list = id_list
+
+    def get_all_products(self):
+        # call product service api here
+        pass
