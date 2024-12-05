c79ec1bf10b03999b72a6b10505af094515cd219
hardikanbati <hardikambati69@gmail.com>
2024-12-05 09:40:06 +0530

    validator

--- api_server/helpers/validators.py
+++ api_server/helpers/validators.py
@@ -1,9 +1,22 @@
 from fastapi import (
     Query,
+    Depends,
+    HTTPException,
+)
+from api.models import (
+    User,
+)
+from db import get_db
+from sqlalchemy.orm import (
+    Session,
 )
 
 
-async def validate_user_key(key: str = Query(max_length=50)):
-    # TODO : check key validation
-    user_key = None
-    return user_key
+async def validate_user_key(key: str = Query(max_length=50), db: Session = Depends(get_db)) -> str:
+    """checks user unique key validation"""
+    query = db.query(User).filter(
+        User.unique_id == key
+    ).first()
+    if not query:
+        raise HTTPException(detail="Invalid key", status_code=400)
+    return query.unique_id
